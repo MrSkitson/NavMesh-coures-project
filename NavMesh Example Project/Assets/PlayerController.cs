9e4679c6f7637f77f1bba4e1eaea65906f2ac2bb
MrSkitson <91326600+MrSkitson@users.noreply.github.com>
2022-01-25 14:26:56 +0100

    Second
    Add AI logic in PlayerController script Now Player going to move when was click mouse on the ground.
    Change configures NavMesh agent

--- NavMesh Example Project/Assets/PlayerController.cs
+++ NavMesh Example Project/Assets/PlayerController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera cam;
+    public NavMeshAgent agent;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+           Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+           RaycastHit hit;
+
+           if (Physics.Raycast(ray, out hit))
+           {
+               // Move our Agent
+               agent.SetDestination(hit.point);
+           }
+
+        }
     }
 }
